4c946affdc9546197d1344e9ceec5672073d61c9
Ashura09 <jankonstanta@gmail.com>
2022-02-09 19:17:00 +0800

    Initial commit

---
+++ menu.js
@@ -0,0 +1,13 @@
+function rosnd()
+{
+    var rolloversound = new Audio("buttonrollover.wav");
+    rolloversound.play();
+    return 0
+}
+
+function btnsnd()
+{
+    var presssound = new Audio("buttonclickrelease.wav");
+    presssound.play();  
+    return true
+}
